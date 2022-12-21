edaa3bd7eaceb25d77669437daf1c8c8dc65a1ba
ShupeiLi <shupei.tina.li@gmail.com>
2022-12-21 22:16:38 +0100

    Train / test split

--- code/model.py
+++ code/model.py
@@ -3,9 +3,12 @@
 import os
 import pandas as pd
 from transformers import BertTokenizer
+from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
+
 dir_path = '../../semeval-2017-tweets_Subtask-A/downloaded/'
+
 
 def clean_data():
     one_file = dir_path + 'twitter-2016test-A.tsv'
@@ -22,14 +25,21 @@ def clean_data():
 
 
 class BertModel():
-    """Extract text embeddings with Bert."""
-    def __init__(self, dir_path):
+    """Extract text embeddings with Bert.
+
+    Args:
+        dir_path: Path of datasets.
+        seed: Random seed.
+    """
+    def __init__(self, dir_path, seed=42):
         self.dir_path = dir_path
+        self.seed = seed
         self.load_data()
 
     def load_data(self):
         col_name = ['id', 'label', 'text']
-        self.train = pd.DataFrame(columns=col_name)
+        data = pd.DataFrame(columns=col_name)
+
         for file in os.listdir(self.dir_path):
             if file != 'twitter-2016test-A.tsv':
                 one_df = pd.read_table(self.dir_path + file, 
@@ -37,16 +47,13 @@ class BertModel():
                                        names=col_name,
                                        index_col=False)
                 print(f'{file}: {one_df.shape[0]}')
-                if file != 'test.tsv':
-                    self.train = pd.concat([self.train, one_df])
-                else:
-                    self.test = one_df
+                data = pd.concat([data, one_df])
 
+        self.train, self.test = train_test_split(data, test_size=0.2, random_state=self.seed)
         print(f'The number of train: {self.train.shape[0]}.')
         print(f'The number of test: {self.test.shape[0]}.')
 
 
 if __name__ == '__main__':
 #   clean_data()
-#   model = BertModel(dir_path)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+    model = BertModel(dir_path)
