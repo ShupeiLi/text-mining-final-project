e58657ee8bec497ae646ce5804d2dbc860cf84f8
ShupeiLi <shupei.tina.li@gmail.com>
2022-12-23 21:46:47 +0100

    Update code

--- code/model.py
+++ code/model.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 
 import os
+import random
 import evaluate
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, recall_score, f1_score, classification_report
 import tensorflow as tf
 from transformers import AutoTokenizer, DataCollatorWithPadding, create_optimizer, TFAutoModelForSequenceClassification
 from transformers.keras_callbacks import KerasMetricCallback, PushToHubCallback
 from datasets import load_dataset
 
 
-#dir_path = '../../semeval-2017-tweets_Subtask-A/downloaded/'
-dir_path = '../data/'
+dir_path = '../../semeval-2017-tweets_Subtask-A/downloaded/'
+#dir_path = '../data/'
 
 os.environ['TOKENIZERS_PARALLELISM'] = 'false'
 
@@ -38,13 +40,14 @@ class BertModel():
         dir_path: Path of datasets.
         tune: Hyperparameter tuning mode. Default: False.
         batch: Batch size. Default: 16.
-        epoch: The number of epoch. Default: 5.
+        epoch: The number of epoch. Default: 50.
         seed: Random seed. Default: 42.
     """
     id2label = {0: 'negative', 1: 'neutral', 2: 'positive'}
     label2id = {'negative': 0, 'neutral': 1, 'positive': 2}
+    names = ['bert-base-cased', 'roberta-base', 'distilbert-base-cased']
     
-    def __init__(self, dir_path, tune=False, batch=16, epoch=5, seed=42):
+    def __init__(self, dir_path, tune=False, batch=16, epoch=50, seed=42):
         self.dir_path = dir_path
         self.batch = batch
         self.epoch = epoch
@@ -102,7 +105,7 @@ class BertModel():
         predictions = np.argmax(predictions, axis=1)
         return self.accuracy.compute(predictions=predictions, references=labels)
 
-    def _bert(self, lr=1e-5, bert_type='bert-base-cased', info=''):
+    def bert(self, lr=1e-5, bert_type='bert-base-cased', info=''):
         """The template for Bert models."""
         tokenizer = AutoTokenizer.from_pretrained(bert_type)
         def preprocess_function(examples):
@@ -130,24 +133,85 @@ class BertModel():
                 )
         model.compile(optimizer=optimizer)
         metric_callback = KerasMetricCallback(metric_fn=self._compute_metrics, eval_dataset=tf_test_set)
-        model.fit(x=tf_train_set, epochs=self.epoch, callbacks=metric_callback)
+        early_stopping_callback = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=3)
+        model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+                filepath=f'../models/{bert_type}', save_weights_only=True, monitor='accuracy', save_best_only=True
+                )
+        callbacks = [metric_callback, early_stopping_callback, model_checkpoint_callback]
+        history = model.fit(x=tf_train_set, epochs=self.epoch, callbacks=callbacks)
+        model.load_weights(f'../models/{bert_type}')
         preds = model.predict(tf_test_set)
         preds = np.argmax(preds.logits, axis=1)
         np.save(f'../results/{bert_type + info}.npy', preds)
+        return history
 
     def tuning(self, bert_type='bert-base-cased'):
-        epochs = [10, 20, 30]
-        lrs = [1e-5, 1e-4, 1e-3]
-        batchs = [16, 32]
-        for epoch in epochs:
-            self.epoch = epoch
-            for batch in batchs:
-                self.batch = batch
-                for lr in lrs:
-                    self._bert(lr=lr, bert_type=bert_type, info=f'_{epoch}_{batch}_{lr}')
+        """Hyperparameter tuning."""
+        lrs = [1e-5, 1e-4, 1e-3, 1e-2]
+        for lr in lrs:
+            history = self.bert(lr=lr, bert_type=bert_type, info=f'_{lr}')
+            his_df = pd.DataFrame.from_dict(history.history)
+            his_df.to_csv(f'../results/{bert_type}_{lr}.csv', index=False)
+            
+    def proposed(self):
+        """Proposed model."""
+        def vote(a, b, c):
+            vote_dict = {0: 0, 1: 0, 2: 0}
+            lst = [a, b, c]
+            for element in lst:
+                for key in list(vote_dict.keys()):
+                    if element == key:
+                        vote_dict[key] += 1
+            for item in list(vote_dict.items()):
+                if item[1] >= 2:
+                    return item[0]
+            return random.sample(list(vote_dict.keys()), 1)[0]
+        preds_dict = dict()
+        for name in BertModel.names:
+            preds = np.load(f'../results/{name}.npy')
+            preds_dict[name] = preds.tolist()
+        preds_df = pd.DataFrame.from_dict(preds_dict)
+        preds = preds_df.apply(lambda row : vote(row['bert-base-cased'], row['roberta-base'], row['distilbert-base-cased']), axis=1).to_numpy()
+        trues = np.array(self.data['test']['label'])
+        print(f"""
+        Proposed
+        Acc: {accuracy_score(trues, preds)}
+        Recall: {recall_score(trues, preds, average='macro')}
+        F1: {f1_score(trues, preds, average='macro')}
+        Report:{classification_report(trues, preds)}
+                """)
+
+    def main(self, bert_type='bert-base-cased'):
+        """Run Bert models."""
+        history = self.bert(bert_type=bert_type)
+        his_df = pd.DataFrame.from_dict(history.history)
+        his_df.to_csv(f'../results/{bert_type}.csv', index=False)
+
+    def evaluation(self):
+        """Model evaluation."""
+        trues = np.array(self.data['test']['label'])
+        for name in BertModel.names:
+            preds = np.load(f'../results/{name}.npy')
+            print(f"""
+            {name}
+            Acc: {accuracy_score(trues, preds)}
+            Recall: {recall_score(trues, preds, average='macro')}
+            F1: {f1_score(trues, preds, average='macro')}
+            Report:{classification_report(trues, preds)}
+                    """)
+
 
 if __name__ == '__main__':
 #   clean_data()
-    model = BertModel(dir_path, tune=True)
-    # !!! TODO: "roberta-base", "distilbert-base-cased"
-    model.tuning()
+   
+#   model = BertModel(dir_path, tune=True)
+#   model.tuning()
+#   model.tuning(bert_type='roberta-base')
+#   model.tuning(bert_type='distilbert-base-cased')
+
+    model = BertModel(dir_path)
+#   model.main()
+#   model.main(bert_type='roberta-base')
+#   model.main(bert_type='distilbert-base-cased')
+#   model.evaluation()
+#   model.proposed()
